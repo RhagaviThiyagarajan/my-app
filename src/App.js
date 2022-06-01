4c998542db2ba04ee7a80febeae224c3b982d211
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-06-02 00:09:54 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,72 +1,87 @@
-import logo from './logo.svg';
+      import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  const name=["Rhagavi","Swathi","Anu"];
-
-  const students=[
-   { name="Rhagavi"
-    pic="https://c.stocksy.com/a/JT7300/z9/743709.jpg?1573689277"/>
-  },
-  { name="Swathi"
-    pic="https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg-1024x683.jpg"/>
-  },
-  { 
-    name="Anu"
-    pic="https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg-1024x683.jpg"/>
-      </div> */}
-    },
-  ];
-
-   /* <Message name="Rhagavi"
-    pic="https://c.stocksy.com/a/JT7300/z9/743709.jpg?1573689277"/>
-
-   <Message name="Swathi"
-    pic="https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg-1024x683.jpg"/>
-
-   <Message name="Anu"
-    pic="https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg-1024x683.jpg"/>
-      </div> */
-      /* {/* // <header className="App-header">
-      //   <img src={logo} className="App-logo" alt="logo" />
-      //   <p> */
-      /* //     Edit <code>src/App.js</code> and save to reload.
-      //   </p> */
-      {/* //   <a */} 
-      {/* //     className="App-link"
-      //     href="https://reactjs.org"
-      //     target="_blank"
-      //     rel="noopener noreferrer"
-      //   //> */}
-     // {/* //     Learn React */}
-      {/* //   </a> */}
-      {/* // </header> */}
-   
-//   //;
-// //
-return(
-  <div className="App">
-    {students.map((student)=>(<Message name={student.name}pic={student.pic}/>
-    ))}
-</div>
-  );
-    }
-export default App;
-function Message(props)
+import { Component } from 'react';
+function App()
 {
-  const name=(props);
-console.log(props);
-  return(
-    <div>
-      <img className='profilepic' src={props.pic} alt={props.name}/>
-      
-    <h1>Hello, {props.name}🎉🎉🤩</h1>
-    </div> 
+// const name="Rhagavi"
+// const time=30;
+const names=["Rhagavi","swathi","Anu","Pooja"];
+const students=[
+{
+  nm:"Rhagavi",
+  pic:"https://thumbs.dreamstime.com/z/happy-blonde-woman…t-happy-blonde-woman-fashion-golden-141968924.jpg"
   
- 
+},
+{
+  nm:"Swathi",
+  pic :"https://thumbs.dreamstime.com/z/profile-view-happy…tudio-shot-against-white-background-176896120.jpg"
+},
+{
+  nm:"Anu",
+  pic:"https://www.designbolts.com/wp-content/uploads/2019/02/girl-laughing-smiling-free-stock-photo-1.jpg"
 
-  );
+},
+{
+  nm:"Pooja",
+  pic:"https://thumbs.dreamstime.com/z/side-view-attractive-happy-woman-making-add-170901128.jpg"
+},
+];
+
+
+return(
+  <div className='App'>
+    
+    
    
+{
+students.map(student=>
+<Welcome names={student.nm} pic={student.pic}/>)
+}
 
+ {/* {
+ names.map(nm=>
+ <Message names={nm}/>)
+}  */}
+
+
+   
+</div>
+);
 
 }
+//props are the way to pass arg to components
+
+export default App;
+ 
+//create Component  
+ //func becoming component
+
+ //first letter should be component
+
+// it should return one and only jsx elem
+ 
+   //defined the component 
+ //logic + view part
+ function Message(pic,name)
+ {
+   console.log(pic,name);
+   // const name=props.name;
+   return( 
+   <div>
+     <img className="profile-pic"src={pic} alt={name}/>
+ 
+ <h1>Hello,{name}🎉</h1>
+ </div>)
+ }
+
+ function Welcome(props)
+ {
+    console.log(props);
+   // const name=props.name;
+   return( 
+   <div>
+     <img className="profile-pic"src={props.pic} alt={props.names}/>
+ 
+ <h1>Hello,{props.names  }🎉</h1>
+ </div>)
+ }
