a4d4879be56db3c50771c8aba2f7dab2022b675c
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-06-11 12:25:35 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,87 +1,174 @@
-      import logo from './logo.svg';
-import './App.css';
-import { Component } from 'react';
-function App()
-{
-// const name="Rhagavi"
-// const time=30;
-const names=["Rhagavi","swathi","Anu","Pooja"];
-const students=[
-{
-  nm:"Rhagavi",
-  pic:"https://thumbs.dreamstime.com/z/happy-blonde-woman…t-happy-blonde-woman-fashion-golden-141968924.jpg"
-  
-},
-{
-  nm:"Swathi",
-  pic :"https://thumbs.dreamstime.com/z/profile-view-happy…tudio-shot-against-white-background-176896120.jpg"
-},
-{
-  nm:"Anu",
-  pic:"https://www.designbolts.com/wp-content/uploads/2019/02/girl-laughing-smiling-free-stock-photo-1.jpg"
 
-},
-{
-  nm:"Pooja",
-  pic:"https://thumbs.dreamstime.com/z/side-view-attractive-happy-woman-making-add-170901128.jpg"
-},
-];
+import "./App.css";
+
+import { useState } from "react";
 
 
-return(
-  <div className='App'>
+
+export function App()
+ {
+ const student=[
+     {
+     name:"Rhagavi",
+     pic:"https://media.istockphoto.com/photos/profile-portr…=0&h=bOle9KnZYM0jSPTLZBaYUIXAYyd5ejl8rzmKd1i_hL8="
+     }
+ ];
+
+ const movieList=
+ [
+    {
+    name: "RRR",
+    poster:
+    "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG",
+    rating: 8.8,
+    summary:
+    "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.",
     
+    },
+    {
+    name: "Iron man 2",
+    poster:
+    "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
+    rating: 7,
+    summary:
+    "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
     
-   
+    },
+    {
+    name: "No Country for Old Men",
+    poster:
+    "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
+    rating: 8.1,
+    summary:
+    "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
+    
+    },
+    {
+    name: "Jai Bhim",
+    poster:
+    "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
+    summary:
+    "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
+    rating: 8.8,
+    
+    },
+    {
+    name: "The Avengers",
+    rating: 8,
+    summary:
+    "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
+    poster:
+    "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
+    
+    },
+    {
+    name: "Interstellar",
+    poster: "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
+    rating: 8.6,
+    summary:
+    "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.",
+    
+    },
+    {
+    name: "Baahubali",
+    poster: "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
+    rating: 8,
+    summary:
+    "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
+    
+    },
+    {
+    name: "Ratatouille",
+    poster:
+    "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
+    rating: 8,
+    summary:
+    "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
+    
+    }
+    ];
+
+
+ 
+
+ return(
+     <div className='App'>
+
 {
-students.map(student=>
-<Welcome names={student.nm} pic={student.pic}/>)
+movieList.map((mv)=>(
+    <Movie movie={mv}
+/>))
 }
 
- {/* {
- names.map(nm=>
- <Message names={nm}/>)
-}  */}
+<Movie/>
+  <Counter/>
+     </div>
+ );
+ }
+ //hook-make hook listen to the change.
+ //hook-function
+ // start with the word called use
+ //setState is again a func-to change.To upgrade the state.
+ //informing react that state is changed.
+
+ //useState-informing react that state is changed.
+ //state-currentValue 
 
 
-   
-</div>
-);
+function Counter()
+{
+    
+const [like,setLike]=useState(0);
+const [dislike,setDisLike]=useState(0);
+//state-current scenario
 
+    //onClick-Camel Case
+    return(
+        <div className='counters-container'>
+           <button className='like-button' onClick={()=>setLike(like+1)
+               
+           }>
+               {like}👍
+           </button> 
+            
+            <button className='dislike-button' onClick={()=>setDisLike(dislike+1)}>
+                {dislike}👎
+                </button>    
+               </div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+ 
+
+    );
 }
-//props are the way to pass arg to components
+
+function Message({pic,name})
+{
+    return(
+        <div>
+        <img className='profile-pic' src={pic} alt={name} />
+            <h1>Hello , {name} you are 25 years old❤{pic}</h1>
+            <Counter/>
+        </div>
+    );
+}
+
+
+
+function Movie({movie})
+{
+    
+  return(
+        <div className='movie-container'>
+      <img src={movie.poster} alt={movie.name} className="movie-poster"/>
+
+         <div className='movie-specs'>
+         <h2 className='movie-name'>{movie.name}</h2>
+         <p className='movie-rating'>⭐{movie.rating}</p>
+         </div>
+
+        <p className='movie-summary'>{movie.summary}</p>
+        </div>
+    );
+    
+}
 
 export default App;
- 
-//create Component  
- //func becoming component
-
- //first letter should be component
-
-// it should return one and only jsx elem
- 
-   //defined the component 
- //logic + view part
- function Message(pic,name)
- {
-   console.log(pic,name);
-   // const name=props.name;
-   return( 
-   <div>
-     <img className="profile-pic"src={pic} alt={name}/>
- 
- <h1>Hello,{name}🎉</h1>
- </div>)
- }
-
- function Welcome(props)
- {
-    console.log(props);
-   // const name=props.name;
-   return( 
-   <div>
-     <img className="profile-pic"src={props.pic} alt={props.names}/>
- 
- <h1>Hello,{props.names  }🎉</h1>
- </div>)
- }
